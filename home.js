d47133668c7229f9fc3722c07de56cbf57a2355b
JamesAbad Webflow/Wized/Xano Expert <jamesabad28@gmail.com>
2023-10-21 08:38:56 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,19 +1,20 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 64,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-btn-next",
-    prevEl: ".swiper-btn-prev",
-  },
-
-  breakpoints: {
-    // when window width is >= 320px
-    992: {
-      slidesPerView: 2,
-      spaceBetween: 64,
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 64,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-btn-next",
+      prevEl: ".swiper-btn-prev",
     },
-  },
+    breakpoints: {
+      // when window width is >= 320px
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 64,
+      },
+    },
+  });
 });
